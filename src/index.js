bf6d4a0787557813cc804bc2bd5f17cca95f5176
1140480798 <73691756+1140480798@users.noreply.github.com>
2021-11-12 11:23:27 +0800

    11/12

--- src/index.js
+++ src/index.js
@@ -5,12 +5,26 @@ import './index.css';
 // Square是一个React组件类，通过接受一些props
 // 被封装好了，可以直接使用，来构建复杂的UI界面
 class Square extends React.Component {
+  // 在所有含有constructor的构造函数的组件，都需要用super(props)开头
+  constructor(props) {
+    super(props);
+    // 通过state来实现"记忆"功能
+    this.state = {
+      value: null,
+    };
+  }
   // 通过render方法展示出来
   // 本质上render方法返回了React元素
   render() {
     return (
-        <button className="square">
-          {this.props.value}
+        <button
+            className="square"
+            onClick={() => {
+              this.setState({value: 'X'})
+            }}
+        >
+          {/*记录自己state属性的结果*/}
+          {this.state.value}
         </button>
     );
   }
@@ -19,6 +33,7 @@ class Square extends React.Component {
 // 渲染了9个方格
 class Board extends React.Component {
   renderSquare(i) {
+    // 传递给子组件，子组件通过props来接受
     return <Square value={i}/>
   }
   render() {
