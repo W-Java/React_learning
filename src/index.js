c43025286c273e504638259a0fd86ded5ed3de0b
1140480798 <73691756+1140480798@users.noreply.github.com>
2021-11-12 16:37:21 +0800

    11/12

--- src/index.js
+++ src/index.js
@@ -5,26 +5,17 @@ import './index.css';
 // Square是一个React组件类，通过接受一些props
 // 被封装好了，可以直接使用，来构建复杂的UI界面
 class Square extends React.Component {
-  // 在所有含有constructor的构造函数的组件，都需要用super(props)开头
-  constructor(props) {
-    super(props);
-    // 通过state来实现"记忆"功能
-    this.state = {
-      value: null,
-    };
-  }
   // 通过render方法展示出来
   // 本质上render方法返回了React元素
   render() {
     return (
         <button
             className="square"
-            onClick={() => {
-              this.setState({value: 'X'})
-            }}
+            // 直接触发Board中的handleClick()
+            onClick={() => this.props.onClick()}
         >
           {/*记录自己state属性的结果*/}
-          {this.state.value}
+          {this.props.value}
         </button>
     );
   }
@@ -32,9 +23,27 @@ class Square extends React.Component {
 
 // 渲染了9个方格
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // 建立一个长度为9的空数组
+      squares: Array(9).fill(null)
+    };
+  }
+  处理子组件squares的点击事件
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares})
+  }
   renderSquare(i) {
-    // 传递给子组件，子组件通过props来接受
-    return <Square value={i}/>
+    // 传递给子组件Square，数组中记录了每一个格子的状态
+    return (
+        <Square
+          value={this.state.squares[i]}
+          onClick={() => this.handleClick(i)}
+        />
+    );
   }
   render() {
     const status = 'Next player: X';
